8807b33caed75739ddcd6c97e55629c6b3b9fba9
Vivek Beniwal <vivek.beniwal@gmail.com>
2014-12-18 20:46:44 -0800

    Vivek Beniwal <vivek.beniwal@gmail.com> import

--- sdl/SdlHelperFunctions.cmake
+++ sdl/SdlHelperFunctions.cmake
@@ -304,6 +304,23 @@ endfunction(xmt_add_executable_explicit)
 
 
 
+  add_custom_target(${TARGET_NAME} ALL COMMAND ${MAVEN_EXECUTABLE} -Dmaven.project.build.directory=${work_dir} -fae -B -q -f pom.xml -Dmaven.test.skip=true clean compile package
+
+
+
+
+function(xmt_maven_project_with_profile TARGET_NAME BINARY_DIR PROFILE_NAME)
+
+  message(STATUS "Run mvn package: \"${MAVEN_EXECUTABLE}\" wth profile:\"${PROFILE_NAME}\"")
+
+  add_custom_target(${TARGET_NAME} ALL COMMAND ${MAVEN_EXECUTABLE} -P${PROFILE_NAME} -Dmaven.project.build.directory=${work_dir} -fae -B -q -f pom.xml -Dmaven.test.skip=true clean compile package
+
+
+
+
+
+
+    -Dmaven.project.build.directory=${work_dir} -Dtest_Dir=${CMAKE_CURRENT_SOURCE_DIR}/src/test/unit
 
 
 
@@ -313,27 +330,10 @@ endfunction(xmt_add_executable_explicit)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function(xmt_maven_project_with_profile_test TARGET_NAME BINARY_DIR PROFILE_NAME)
+  xmt_maven_project_with_profile(${TARGET_NAME} ${BINARY_DIR} ${PROFILE_NAME})
+  xmt_maven_test(${TARGET_NAME} ${BINARY_DIR})
+endfunction(xmt_maven_project_with_profile_test)
 
 
 
