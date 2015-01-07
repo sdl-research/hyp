eb8834bdcbfcc0e41d34296c62b6363b06e7fb5f
graehl <graehl@gmail.com>
2015-01-07 09:12:35 -0800

    use boost gzstream instead of LGPL gzstream.h
    from SDL: 56120d991801c5be93a77e422895027a49d131ee
    
        Change-Id: I9e3bb663a8cad1f9c6e52aab52930ce79c3e2fd2

--- sdl/CMakeConfig.hpp
+++ sdl/CMakeConfig.hpp
@@ -1,6 +1,6 @@
 #ifndef LW__CMAKECONFIG_HPP
 #define LW__CMAKECONFIG_HPP
 
-#define PROJECT_SOURCE_DIR "/local/xmt/hyp/sdl"
+#define PROJECT_SOURCE_DIR "/Users/graehl/c/hyp/sdl"
 
 #endif
--- sdl/Util/Delete.hpp
+++ sdl/Util/Delete.hpp
@@ -1,17 +1,16 @@
 // Copyright 2014 SDL plc
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 /** \file
 
     RAII scope-guarded exception safe automatic malloc/free, and delete
@@ -174,6 +173,7 @@ struct AutoDeleteAll : std::vector<T*> {
   void releaseAll() { Base::clear(); }
   AutoDeleteAll() {}
   AutoDeleteAll(T* toDelete) : Base(1, toDelete) {}
+  AutoDeleteAll(std::size_t n) : Base(n) {}
   ~AutoDeleteAll() {
     for (typename Base::const_iterator i = Base::begin(), e = Base::end(); i != e; ++i) delete *i;
   }
