d39f388f9cd283bcfa2f035f399b466407c30173
graehl <graehl@gmail.com>
2016-07-08 22:53:47 -0700

    cmake

--- sdl/cmake/Shared/FindYamlCpp.cmake
+++ sdl/cmake/Shared/FindYamlCpp.cmake
@@ -6,34 +6,17 @@
 # YAML_CPP_LIBRARY - yaml-cpp library
 
 unset(YAML_CPP_INCLUDE_DIR CACHE)
-sdl_find_path(YAML_CPP_INCLUDE_DIR yaml-cpp/yaml.h PATHS ${YAML_CPP_ROOT}/include)
+sdl_find_path(YAML_CPP_INCLUDE_DIR yaml-cpp/yaml.h PATHS ${YAML_CPP_ROOT}/include ${YAML_CPP_SHARED_ROOT}/include)
+#sdl_find_path(YAML_CPP_INCLUDE_DIR yaml-cpp/yaml.h PATHS ${YAML_CPP_ROOT}/include)
 
 unset(YAML_CPP_LIBRARY_RELEASE CACHE)
 sdl_find_library(YAML_CPP_LIBRARY_RELEASE yaml-cpp PATHS ${YAML_CPP_ROOT}/lib)
-if(WIN32)
-  unset(YAML_CPP_LIBRARY_DEBUG CACHE)
-  sdl_find_library(YAML_CPP_LIBRARY_DEBUG NAMES yaml-cppd PATHS ${YAML_CPP_ROOT}/lib NO_DEFAULT_PATH)
-  if(YAML_CPP_INCLUDE_DIR AND YAML_CPP_LIBRARY_RELEASE AND YAML_CPP_LIBRARY_DEBUG)
-    set(YAML_CPP_FOUND 1)
-
-    set(YAML_CPP_LIBRARY
-       optimized ${YAML_CPP_LIBRARY_RELEASE}
-       debug ${YAML_CPP_LIBRARY_DEBUG}
-       )
-  else()
-    set(YAML_CPP_FOUND 0 CACHE BOOL "Not found yaml-cpp library")
-    message(WARNING "Library 'yaml-cpp' not found (searched in ${YAML_CPP_ROOT}).")
-  endif()
+if(YAML_CPP_INCLUDE_DIR AND YAML_CPP_LIBRARY_RELEASE)
+  set(YAML_CPP_FOUND 1)
+  set(YAML_CPP_LIBRARY ${YAML_CPP_LIBRARY_RELEASE})
 else()
-  if(YAML_CPP_INCLUDE_DIR AND YAML_CPP_LIBRARY_RELEASE)
-    set(YAML_CPP_FOUND 1)
-    set(YAML_CPP_LIBRARY ${YAML_CPP_LIBRARY_RELEASE})
-  else()
-    set(YAML_CPP_FOUND 0 CACHE BOOL "Not found yaml-cpp library")
-    message(WARNING "Library 'yaml-cpp' not found (searched in ${YAML_CPP_ROOT}).")
-  endif()
-
+  set(YAML_CPP_FOUND 0 CACHE BOOL "Not found yaml-cpp library")
+  message(WARNING "Library 'yaml-cpp' not found (searched in ${YAML_CPP_ROOT}).")
 endif()
-
 
 mark_as_advanced(YAML_CPP_INCLUDE_DIR YAML_CPP_LIBRARY)
